correctly checking for dtypes in the various routines using np.issubdtype instead of using the x.dtype.name which sometimes yilds wrong results

mappy/map/MapBase.py
@@ -351,9 +351,6 @@ class MapBase:
                 Index of self._tiles range(0,self._c_tiles)
             [None]
                 Can be used to access the file as a whole
-        crs : {"data", "latlon"}, optional
-            Coordinate system of Index if bounds are used. 'Data' returns the coordinates in the crs of the file, while
-            'latlon' uses EPSG:4326.
 
         Returns
         -------
@@ -416,7 +413,12 @@ class MapBase:
                 if y1 < -90 or y1 > 90:
                     raise ValueError(f"Top coordinate of the globe: {y1}")
 
-                ind = rio.coords.BoundingBox(*bounds_to_data_projection(self._data_proj, ind))
+                bounds1 = bounds_to_data_projection(self._data_proj, (x0, y0, x1, y1))
+                bounds2 = bounds_to_data_projection(self._data_proj, (x0, y1, x1, y0))
+                x = (bounds1[0], bounds1[2], bounds2[0], bounds2[2])
+                y = (bounds1[1], bounds1[3], bounds2[1], bounds2[3])
+
+                ind = rio.coords.BoundingBox(np.min(x), np.min(y), np.max(x), np.max(y))
 
             # add window object to self._tiles list
             temporary_window = rio.windows.from_bounds(*ind, self._file.transform)
@@ -502,7 +504,7 @@ class MapBase:
 
     profile = property(get_file_profile)
 
-    def get_bounds(self, ind=-1, crs="data"):
+    def get_bounds(self, ind=-1):
         """
         return rasterio bounds object of the current window
         
@@ -510,9 +512,6 @@ class MapBase:
         ----------
         ind : .
             see self.get_pointer()
-        crs : {"data", "latlon"}, optional
-            Coordinate system of the returned bounds. Data returns the coordinates in the crs of the file, while
-            'latlon' uses EPSG:4326.
 
         Returns
         -------

mappy/ndarray_functions/rolling_functions.py
@@ -154,7 +154,7 @@ def rolling_sum(a, window_size, reduce=False):
         return cumsum[window_size:] - cumsum[:-window_size]
 
     elif a.ndim == 2 and not reduce:
-        if a.dtype == np.bool_:
+        if np.issubdtype(a.dtype, np.bool_):
             dtype = np.int
         else:
             dtype = a.dtype
@@ -173,7 +173,7 @@ def rolling_sum(a, window_size, reduce=False):
 
     elif a.ndim == 3 and not reduce:
         # todo; check if this is actually faster than the backup solution
-        if a.dtype == np.bool_:
+        if np.issubdtype(a.dtype, np.bool_):
             dtype = np.int
         else:
             dtype = a.dtype

mappy/plotting/misc.py
@@ -30,7 +30,7 @@ def _determine_cmap_boundaries_discrete(m, bins, cmap, clip_legend=False):
     """
     m = np.array(m)
 
-    if 'float' in m.dtype.name:
+    if np.issubdtype(m.dtype, np.floating):
         data = m[~np.isnan(m)]
     else:
         data = m.flatten()
@@ -102,7 +102,7 @@ def _determine_cmap_boundaries_continuous(m, vmin, vmax):
     -------
     norm, extend
     """
-    if m.dtype == np.float:
+    if np.issubdtype(m.dtype, np.floating):
         data = m[~np.isnan(m)]
     else:
         data = m

mappy/plotting/plotting_maps.py
@@ -77,7 +77,7 @@ def plot_map(m, bins=None, bin_labels=None, cmap=None, vmin=None, vmax=None, leg
     """
     if m.ndim not in (2, 3):
         raise ValueError("Input data needs to be 2D or present RGB(A) values on the third axis.")
-    if m.ndim == 3 and (m.shape[-1] not in (3, 4) or m.dtype == "bool_"):
+    if m.ndim == 3 and (m.shape[-1] not in (3, 4) or np.issubdtype(m.dtype, np.bool_)):
         raise ValueError(f"3D arrays are only acceptable if presenting RGB(A) information. Shape: {m.shape}, "
                          f"dtype: {m.dtype}")
 
@@ -109,12 +109,13 @@ def plot_map(m, bins=None, bin_labels=None, cmap=None, vmin=None, vmax=None, leg
         nan_mask = np.isnan(m)
         m = (m > bins[0]).astype(float)
         m[nan_mask] = np.nan
-        ax, legend_ = plot_classified_map(m, colors=['lightgrey', 'red'], labels=[f'< {bins[0]}', f'> {bins[0]}'],
-                                          ax=ax, legend_ax=legend_ax, legend=legend, legend_kwargs=legend_kwargs,
-                                          aspect=aspect, pad_fraction=pad_fraction, force_equal_figsize=False,
-                                          nan_color=nan_color, **kwargs)
+        ax, legend_ = plot_classified_map(m, colors=['lightgrey', 'red'], bins=[0, 1],
+                                          labels=[f'< {bins[0]}', f'> {bins[0]}'], ax=ax, legend_ax=legend_ax,
+                                          legend=legend, legend_kwargs=legend_kwargs, aspect=aspect,
+                                          pad_fraction=pad_fraction, force_equal_figsize=False, nan_color=nan_color,
+                                          **kwargs)
 
-    elif m.dtype != "bool_" and m.ndim == 2:
+    elif not np.issubdtype(m.dtype, np.bool_) and m.ndim == 2:
         if isinstance(bins, type(None)):
             norm, extend = _determine_cmap_boundaries_continuous(m=m, vmin=vmin, vmax=vmax)
             cmap.set_bad(nan_color)

mappy/plotting/plotting_shapes.py
@@ -159,7 +159,7 @@ def plot_shapes(lat=None, lon=None, values=None, s=None, df=None, bins=None, bin
                                              aspect=aspect, pad_fraction=pad_fraction, force_equal_figsize=False,
                                              nan_color=nan_color, **kwargs)
 
-    elif values.dtype != "bool_":
+    elif np.issubdtype(values.dtype, np.bool_):
         if isinstance(legend_kwargs, type(None)):
             if legend == "colorbar" or not legend:
                 legend_kwargs = {}

mappy/raster_functions/focal_statistics.py
@@ -7,7 +7,7 @@ from ..ndarray_functions.rolling_functions import rolling_window, rolling_sum, r
 
 
 def _focal_majority(a, window_size, fraction_accepted, reduce, r, ind_inner, majority_mode):
-    if a.dtype not in ('float32', 'float64'):
+    if not np.issubdtype(a.dtype, np.floating):
         a = a.astype(np.float64)
 
     values = np.unique(a)
@@ -184,7 +184,7 @@ def focal_statistics(a, window_size, *, func=None, fraction_accepted=0.7, verbos
     if func not in list_of_functions:
         raise KeyError("Function not available")
 
-    if a.dtype != np.float64 and func != "majority":
+    if not np.issubdtype(a.dtype, np.floating) and func != "majority":
         # print("input array converted to float")
         a = a.astype(np.float64)
 