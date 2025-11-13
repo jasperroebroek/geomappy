1f56da3b8515e75e7ac1e489666a39f2bc53e99d
jroebroek <roebroek.jasper@gmail.com>
2025-11-13 13:26:35 +0100

    version 0.1.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ copyright = '2021, Jasper Roebroek'
 author = 'Jasper Roebroek'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.1'
 
 
 # -- General configuration ---------------------------------------------------
